389ea8db076f89dc865b7e4c0243c5dcefc597c2
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2013-04-25 15:42:52 +0200

    fixing timeline

--- plugins/NeatlineTime/views/shared/timelines/items.neatlinetime-json.php
+++ plugins/NeatlineTime/views/shared/timelines/items.neatlinetime-json.php
@@ -11,22 +11,14 @@ while (loop_items()) {
 
     
     //get dates
-    //$itemDates = item('Dublin Core', 'Date','all');
-    $neatlineTimeEvent['start'] = '2012-01-01';
-    $neatlineTimeEvent['title'] = $itemTitle;
-    $neatlineTimeEvent['link'] = $itemLink;
-    $neatlineTimeEvent['classname'] = neatlinetime_item_class();
-    $neatlineTimeEvent['description'] = $itemDescription;
-    $neatlineTimeEvents[] = $neatlineTimeEvent;
+    $itemDates = item('Dublin Core', 'Date','all');   
 
     /*if ($file = get_db()->getTable('File')->findWithImages(item('id'), 0)) {
         $fileUrl = file_display_uri($file, 'square_thumbnail'); 
     }*/
-    /*if (!empty($itemDates)) {
+    if (!empty($itemDates)) {
       foreach ($itemDates as $itemDate) {
-            /*
-            $itemDescription .= $itemDate ."<br>";
-
+           
             $neatlineTimeEvent = array();
             $itemDate = preg_replace("/[^0-9]/","", $itemDate);
             if(strlen($itemDate)==4){
@@ -68,38 +60,19 @@ while (loop_items()) {
                 
                 $neatlineTimeEvent['description'] = $itemDescription;
                 $neatlineTimeEvents[] = $neatlineTimeEvent;
+                 * 
+                 */
             }
-       
-            /*$itemDate = $itemDate;
-
-            $neatlineTimeEvent = array();
-            $dateArray = explode('/', $itemDate);
-
-            if ($dateStart = neatlinetime_convert_date(trim($dateArray[0]))) {
-                $neatlineTimeEvent['start'] = $dateStart;
-
-                if (count($dateArray) == 2) {
-                    $neatlineTimeEvent['end'] = neatlinetime_convert_date(trim($dateArray[1]));
-                }
-
-                $neatlineTimeEvent['title'] = $itemTitle;
-                $neatlineTimeEvent['link'] = $itemLink;
-                $neatlineTimeEvent['classname'] = neatlinetime_item_class();
-
-                if ($fileUrl) {
-                    $neatlineTimeEvent['image'] = $fileUrl;
-                }
-
-                $neatlineTimeEvent['description'] = $itemDescription;
-                $neatlineTimeEvents[] = $neatlineTimeEvent;
-            }
+         
+            $neatlineTimeEvent['description'] = $itemDescription;
+            $neatlineTimeEvents[] = $neatlineTimeEvent;
+            
         }
          
             
     }
-}*/
-   
 }
+
 $neatlineTimeArray = array();
 $neatlineTimeArray['dateTimeFormat'] = "iso8601";
 $neatlineTimeArray['events'] = $neatlineTimeEvents;
@@ -107,4 +80,3 @@ $neatlineTimeArray['events'] = $neatlineTimeEvents;
 $neatlinetimeJson = json_encode($neatlineTimeArray);
 
 echo $neatlinetimeJson;
-
