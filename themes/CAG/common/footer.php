a1ff5e0a2bdb411e71511f4e81aff8627d725709
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2014-08-27 12:56:14 +0200

    fix #151

--- themes/CAG/common/footer.php
+++ themes/CAG/common/footer.php
@@ -1,43 +1,43 @@
 </div>
 <div id="footer" class="group">
-	<div class="column nieuwsbrief">
-           
-                <h2>Nieuwsbrief</h2>
+	<div class="column nieuwsbrief">            
+            <h2>Nieuwsbrief</h2>
                 <div id="inschrijven"><a href="<?php echo url("newsletter/index/register")?>">Inschrijven</a></div>                
                 <div class="archief-link"><a href="<?php echo url("nieuwsbriefarchief")?>">Nieuwsbrief archief</a></div>
                 <div class="archief-link"><a href="<?php echo url("newsletter/index/unsubscribe")?>">Uitschrijven nieuwsbrief</a></div>
+                
 	</div><!-- /.column -->
 	<div class="column links">
 		<div class="column left">
-						<h2>Website</h2>
-							<ul>
-							<li><a href="<?php echo url('over/')?>">Over deze website</a></li>
-							<li><a href="mailto:diantha.osseweijer@cagnet.be">Technische problemen</a></li> <!-- link CAG-->
-							<li><a href="<?php echo url('copyright/');?>">Copyright en disclaimer</a></li>
-
-							<!-- <li><a href="<?php echo url("sitemap/");?>">Sitemap</a></li> -->
-							</ul>
+                    <h2>Website</h2>
+                    <ul>
+                    <li><a href="<?php echo url('over/')?>">Over deze website</a></li>
+                    <li><a href="mailto:diantha.osseweijer@cagnet.be">Technische problemen</a></li> <!-- link CAG-->
+                    <li><a href="<?php echo url('copyright/');?>">Copyright en disclaimer</a></li>
+                    </ul>                   					
 		</div><!-- /.column -->
 		<div class="column right">
-						<h2>Organisatie</h2>
-							<ul>
-							<li><a href="<?php echo url('over/missie/'); ?>">Missie en visie</a></li>
-							<li><a href="<?php echo url('werking/'); ?>">Diensten</a></li>
-							<li><a href="<?php echo url('team/'); ?>">Team</a></li>
-							<li><a href="<?php echo url('werking/projecten/'); ?>">Projecten</a></li>
-							<li><a href="<?php echo url('contact/'); ?>">Wegbeschrijving</a></li>
-							<li><a href="<?php echo url('over/partners/'); ?>">Partners</a></li>
-							</ul>
+                    <h2>Communicatie</h2>
+                    <ul>
+                    <li><a href="<?php echo url('bronnen/eigenpublicaties/'); ?>">Publicaties</a></li>
+                    <li><a href="<?php echo url('vacatures/'); ?>">Vacatures</a></li>
+                    <li><a href="<?php echo url("contact/");?>">Contact</a></li>
+                    <li><a href="<?php echo url('wegbeschrijving/'); ?>">Wegbeschrijving</a></li>
+                    <!--
+                    <li><a href="http://www.cagnet.be/showpage.php?pageID=26">English</a></li>
+                    <li><a href="http://www.cagnet.be/showpage.php?pageID=25 ">français</a></li>-->
+                    </ul>
 		</div><!-- /.column -->
 		<div class="column  right ">
-		<h2>Communicatie</h2>
-							<ul>
-							<li><a href="<?php echo url("contact/");?>">Contact</a></li>
-							<li><a href="<?php echo url('nieuwsbriefarchief/'); ?>">Nieuwsbrief</a></li>
-							<li><a href="<?php echo url('bronnen/eigenpublicaties/'); ?>">Publicaties</a></li>
-							<li><a href="http://www.cagnet.be/showpage.php?pageID=26">English</a></li><!-- link CAG-->
-							<li><a href="http://www.cagnet.be/showpage.php?pageID=25 ">français</a></li><!-- link CAG-->
-							</ul>
+                    	
+                     <h2>Over ons</h2>
+                    <ul>
+                    <li><a href="<?php echo url('over/missie/'); ?>">Missie</a></li>
+                    <li><a href="<?php echo url('werking/basiswerking/'); ?>">Basiswerking</a></li>
+                    <li><a href="<?php echo url('over/team/'); ?>">Team</a></li>
+                    <li><a href="<?php echo url('over/team/'); ?>">Projecten</a></li>                       
+                    <li><a href="<?php echo url('over/partners/'); ?>">Partners</a></li>
+                    </ul>	
 		</div>
 	</div><!-- /.column -->
 	<div class="column contact">            
