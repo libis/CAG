fe5494726ba7cf7e3a14e14709a3e35b0ceca48f
joris <joris.lambrechts@bib.kuleuven.be>
2015-06-25 15:19:42 +0200

    fix #209

--- themes/CAG/solr-search/results/index.php
+++ themes/CAG/solr-search/results/index.php
@@ -243,9 +243,9 @@ jQuery(document).ready(function() {
                        $html .=link_to_item("<h4>".metadata($item,array('Dublin Core','Title'))."</h4>",array(),'show',$item).
                        "<p>".metadata($item,array('Dublin Core','Description'),array('snippet'=>50))."</p> </div>";
 
-                       if(strtotime($item->added) > strtotime('1 year ago')){$featured .= $html;}
+                       if(metadata($item,array('Item Type Metadata','recent'))=='ja'){$featured .= $html;}
                        else{
-                        $pub .=$html;                        
+                        $pub .=$html;            
                        }
                    }
                 endforeach;
