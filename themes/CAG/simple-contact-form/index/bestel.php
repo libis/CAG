a7ca64037a29e8b27761d615cc3bb8ea5182434f
Joris <joris.lambrechts@kuleuven.be>
2018-05-22 10:34:01 +0200

    move checkboxes

--- themes/CAG/simple-contact-form/index/bestel.php
+++ themes/CAG/simple-contact-form/index/bestel.php
@@ -111,15 +111,16 @@
 
         });
     </script>
-    <div class="field">
-        <label><?php echo $this->formLabel('newsletter', 'Ik wil graag de nieuwsbrief en/of andere activiteiten van CAG en ICAG ontvangen via mail'); ?></label>
-        <input type="checkbox" name="newsletter">
-    </div>
-    <div class="field">
-        <label><?php echo $this->formLabel('privacy', ''); ?>Ik ga akkoord met de <a href="'.url('copyright').'">privacy statement</a></label>
-        <input type="checkbox" name="privacy">
-    </div>
+
     <fieldset>
+      <div class="field">
+          <label><?php echo $this->formLabel('newsletter', 'Ik wil graag de nieuwsbrief en/of andere activiteiten van CAG en ICAG ontvangen via mail'); ?></label>
+          <input type="checkbox" name="newsletter">
+      </div>
+      <div class="field">
+          <label><?php echo $this->formLabel('privacy', ''); ?>Ik ga akkoord met de <a href="'.url('copyright').'">privacy statement</a></label>
+          <input type="checkbox" name="privacy">
+      </div>
         <?php if ($captcha): ?>
         <div class="field">
             <?php echo $captcha; ?>
